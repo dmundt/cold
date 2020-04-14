269f45ce08def1210a5746f63927527ffdd1c7d2
Daniel Mundt <900458+dmundt@users.noreply.github.com>
2020-04-14 18:19:55 +0200

    First working version

--- cmd/cold/main.go
+++ cmd/cold/main.go
@@ -1,20 +1,32 @@
 package main
 
 import (
-	"github.com/dmundt/go-firmata"
+	"flag"
 	"time"
+
+	"github.com/dmundt/go-firmata"
 )
 
 func main() {
-	c, err := firmata.NewClient("COM4", 57600)
+	port := flag.String("port", "COM1", "serial port")
+	rel := flag.Int("rel", 1, "relay number")
+	dur := flag.Int("dur", 1, "duration in [s]")
+
+	flag.Parse()
+
+	if *rel < 1 || *rel > 4 {
+		panic("Relay pin number is outside of valid range [1..4]")
+	}
+
+	c, err := firmata.NewClient(*port, 57600)
 	if err == nil {
 		defer c.Close()
 
-		c.DigitalWrite(2, true)
-		c.DigitalWrite(3, true)
-		c.DigitalWrite(4, true)
-		c.DigitalWrite(5, true)
-
-		time.Sleep(2 * time.Second)
+		c.Log.Info("Close relay #%d", *rel)
+		c.SetPinMode(byte(*rel)+1, firmata.Output)
+		time.Sleep(time.Duration(*dur) * time.Second)
+		c.Log.Info("Open relay #%d", *rel)
+		c.SetPinMode(byte(*rel)+1, firmata.Input)
+		c.Log.Close()
 	}
 }
