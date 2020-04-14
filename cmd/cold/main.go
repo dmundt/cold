579cc77959c14311d755cf346114a6f9d85347ec
Daniel Mundt <900458+dmundt@users.noreply.github.com>
2020-04-14 21:47:21 +0200

    Add comments

--- cmd/cold/main.go
+++ cmd/cold/main.go
@@ -9,8 +9,9 @@ import (
 
 func main() {
 	port := flag.String("port", "COM1", "serial port")
-	rel := flag.Int("rel", 1, "relay number")
+	rel := flag.Int("rel", 1, "relay number [1..4]")
 	dur := flag.Int("dur", 1, "duration in [s]")
+	rate := flag.Int("rate", 57600, "baud rate")
 
 	flag.Parse()
 
@@ -18,15 +19,18 @@ func main() {
 		panic("Relay pin number is outside of valid range [1..4]")
 	}
 
-	c, err := firmata.NewClient(*port, 57600)
+	// Open firmata connection.
+	c, err := firmata.NewClient(*port, *rate)
 	if err == nil {
 		defer c.Close()
 
+		// Toggle specific relay.
+		pin := *rel + 1
 		c.Log.Info("Close relay #%d", *rel)
-		c.SetPinMode(byte(*rel)+1, firmata.Output)
+		c.SetPinMode(byte(pin), firmata.Output)
 		time.Sleep(time.Duration(*dur) * time.Second)
 		c.Log.Info("Open relay #%d", *rel)
-		c.SetPinMode(byte(*rel)+1, firmata.Input)
+		c.SetPinMode(byte(pin), firmata.Input)
 		c.Log.Close()
 	}
 }
